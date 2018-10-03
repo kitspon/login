8f08d7766b60a164be01bf92699c67754315388d
kitspon <ice_seed_freedom@hotmail.com>
2018-10-03 16:22:46 +0700

    Add files via upload

--- Code.py
+++ Code.py
@@ -2,28 +2,14 @@ import sqlite3
 def login(username, password):
     conn = sqlite3.connect('id.db')
     c = conn.cursor()
-    u = 0
-    p = 0
     if type(username) and type(password) == str:
         for row in c.execute("SELECT username FROM id"):
             if row[0] == username:
-                u = 1
-        for row in c.execute("SELECT password FROM id"):
-            if row[0] == password:
-                p = 1
-        if u == 1:
-            if p == 1:
-                print('Login sucess')
-                return True
-        if u == 0 and p == 1:
-            print('User does not exits but correct password')
-            return False
-        if u == 0:
-            if p == 0:
-                print('User does not exits and wrong password')
+                for row in c.execute("SELECT password FROM id"):
+                    if row[0] == password:
+                        return True
                 return False
-        if u == 1 and p == 0:
-            print('correct Username but wrong password')
-            return False
+        return False
+
         
                 
--- testcode.py
+++ testcode.py
@@ -1,13 +1,13 @@
 import Code
 import unittest
 class Testcode(unittest.TestCase):
-    def test_1(self):
+    def test_CorrectUsernameAndCorrectPassword(self):
         self.assertEqual(Code.login('thirawat', '123456'),True)
-    def test_2(self):
+    def test_CorrectUsernameButIncorrectPassword(self):
         self.assertEqual(Code.login('thirawat', '12356'),False)
-    def test_3(self):
+    def test_IncorrectUsernameButCorrectPassword(self):
         self.assertEqual(Code.login('thiawat', '123456'),False)
-    def test_4(self):
+    def test_IncorrectUsernameAndIncorrectPassword(self):
         self.assertEqual(Code.login('thiraat', '12346'),False)
   
 if __name__ == '__main__':
